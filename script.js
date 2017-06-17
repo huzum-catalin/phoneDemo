0a5e5bf777f26299e1ac38f1830238d86b976b38
Huzum Catalin <huzum.catalin@outlook.com>
2017-06-17 10:58:10 +0300

    =Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+	// create the module and name it scotchApp
+	var scotchApp = angular.module('scotchApp', ['ngRoute']);
+
+	// configure our routes
+	scotchApp.config(function($routeProvider) {
+		$routeProvider
+
+			// route for the home page
+			.when('/', {
+				templateUrl : 'pages/home.html',
+				controller  : 'mainController'
+			})
+
+			// route for the about page
+			.when('/phones', {
+				templateUrl : 'pages/phones.html',
+				controller  : 'phonesController'
+			});
+
+			
+	});
+
+	// create the controller and inject Angular's $scope
+	scotchApp.controller('mainController', function($scope) {
+		// create a message to display in our view
+		$scope.message = 'Everyone come and see how good I look!';
+	});
+
+	scotchApp.controller('phonesController', function($scope) {
+		$scope.message = 'Look! I am an about page.';
+	});
+
+	
